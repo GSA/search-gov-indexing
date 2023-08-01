d2df9daea4e6f35e4d6ca3582589e46f41efb522
David Yao <davidyao217@gmail.com>
2023-08-01 14:37:12 -0400

    Consolidate spiders into one Scrapy project and clean up file structure

--- scrapy_vs_scrutiny/comparison/comparison.py
+++ scrapy_vs_scrutiny/comparison/comparison.py
@@ -8,23 +8,20 @@ import pandas as pd
 
 # to run, enter at the prompt: `python3 comparison.py`
 
-suf = '../scrapy_urls/' # suf is short for scrapy_urls_folder 
-scUf = '../scrutiny_urls/processed' # scUf is short for scrutiny_urls_folder
+suf = '/scrapy_urls/' # suf is short for scrapy_urls_folder 
+scUf = '/scrutiny_urls/processed' # scUf is short for scrutiny_urls_folder
 
-scrapy_paths = [suf + '/test.csv', 
-               suf + '/armymwr_urls.csv', 
+scrapy_paths = [suf + '/armymwr_urls.csv', 
                suf + '/get_smart_about_drugs_urls.csv', 
                suf + '/james_webb_urls.csv',
                suf + '/travel_dod_mil_urls.csv',
                suf + '/veteran_affair_urls.csv']
-scrutiny_paths = [scUf + '/test.txt',
-                scUf + '/armymwr.txt', 
+scrutiny_paths = [scUf + '/armymwr.txt', 
                 scUf + '/get_smart_about_drugs.txt', 
                 scUf + '/james_webb.txt',
                 scUf + '/travel_dod_mil.txt',
                 scUf + '/veteran_affairs.txt']
-text_gaps = ['test',
-             'armymwr',
+text_gaps = ['armymwr',
              'get smart about drugs',
              'james webb',
              'travel.dod.mil',
@@ -42,13 +39,6 @@ for i in range(0, len(scrapy_paths)):
         writer.writerow(pd.Series(list(url_sets[1])))
         writer.writerow(pd.Series(list(url_sets[2])))
     csvfile.close()
-    """
-    df = pd.DataFrame()
-    df['Scrutiny Exclusive'] = pd.Series(list(url_sets[0]))
-    df['Scrapy Exclusive'] = pd.Series(list(url_sets[1]))
-    df['Common'] = pd.Series(list(url_sets[2]))
-    print(df)
-    """
 
 
 
